dce6088bd4771979cb43bba8ac6d0374227e0347
jordan <jordan.g.burk@gmail.com>
2016-07-16 18:26:00 -0700

    rudimentary physics

--- bundle.js
+++ bundle.js
@@ -53,17 +53,18 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Canvas = __webpack_require__(2);
-	const Wave = __webpack_require__(4);
-	const Color = __webpack_require__(3);
+	const Player = __webpack_require__(2);
+	const Canvas = __webpack_require__(3);
+	const Wave = __webpack_require__(5);
+	const Color = __webpack_require__(4);
 	
 	// global singleton canvas, or too dangerous?
 	
 	function Game(){
 	  this.canvas = new Canvas;
 	  const wave = new Wave(this.canvas);
-	  wave.addPlayer();
 	  this.wave = wave;
+	  this.player = new Player(wave);
 	}
 	
 	Game.init = function(){
@@ -74,10 +75,17 @@
 	Game.prototype.render = function(){
 	  this.canvas.render();
 	  this.wave.render();
+	  this.player.render(this.canvas.ctx);
+	};
+	
+	Game.prototype.move = function () {
+	  this.wave.move();
+	  this.player.move();
 	};
 	
 	Game.prototype.run = function(){
 	  Color.step();
+	  this.move();
 	  this.render();
 	  window.requestAnimationFrame(() => this.run());
 	};
@@ -89,7 +97,98 @@
 /* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Color = __webpack_require__(3);
+	const Color = __webpack_require__(4);
+	const Listener = __webpack_require__(7);
+	
+	function Player(wave){
+	  this.wave = wave;
+	  this.x = 400;
+	  this.y = 400;
+	  this.speed = 1.5;
+	  this.grounded = true;
+	  this.fallSpeed = 0;
+	  this.fallTime = 0;
+	}
+	
+	Player.prototype.render = function (ctx) {
+	  ctx.fillStyle = Color.wave();
+	  ctx.beginPath();
+	  ctx.ellipse(this.x, this.y, 20, 20, 0, 0, Math.PI * 2, false);
+	  ctx.fill();
+	};
+	
+	Player.prototype.move = function () {
+	  console.log(this.speed);
+	  const waveData = this.waveData();
+	  if(this.grounded){
+	    this.accelerate(waveData);
+	  } else {
+	    this.fall(waveData);
+	  }
+	
+	  // if(this.grounded) this.x -= this.wave.speed;
+	  this.x += this.speed;
+	};
+	
+	Player.prototype.fall = function (waveData) {
+	  this.fallTime += .1;
+	  this.fallSpeed += this.fallTime;
+	  this.y += this.fallSpeed;
+	
+	  if(this.y > waveData[0]){
+	    this.y = waveData[0];
+	    this.grounded = true;
+	    this.fallSpeed = 0;
+	    this.fallTime = 0;
+	  }
+	};
+	
+	Player.prototype.accelerate = function (waveData) {
+	  if(this.speed < 2) this.speed += .1;
+	
+	
+	  if(waveData[1] > .2){
+	    // actually, we want to check slope vs slope
+	    // HWratio vs ratio of fallspeed to speed
+	
+	    this.grounded = false;
+	  } else {
+	    this.y = waveData[0];
+	    if(this.speed > 0) this.speed += waveData[1];
+	  }
+	};
+	
+	Player.prototype.waveData = function () {
+	  for(let i = 0; i < this.wave.points.length; i++){
+	    const point = this.wave.points[i];
+	    if(point.x > this.x){
+	      const prevPoint = this.wave.points[i-1];
+	
+	      const total = Math.abs(point.x - prevPoint.x)
+	      const left = Math.abs(this.x - prevPoint.x);
+	      const right = Math.abs(this.x - point.x);
+	      const leftWeight = right / total; // opposite on purpose
+	      const rightWeight = left / total; // closer should mean bigger, not smaller
+	
+	      // this.tilt = (Math.PI / 2) * heightWidthRatio * (leftWeight < rightWeight ? leftWeight : rightWeight);
+	
+	      // negaative HWR means uphill
+	      const heightWidthRatio = (point.y - prevPoint.y) / (point.x - prevPoint.x);
+	      const y = (prevPoint.y * leftWeight + point.y * rightWeight);
+	
+	      return [y, heightWidthRatio];
+	    }
+	  }
+	};
+	
+	module.exports = Player;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Color = __webpack_require__(4);
 	
 	function Canvas(){
 	  this.self = document.getElementById("canvas")
@@ -115,7 +214,7 @@
 
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	function Color(){
@@ -127,30 +226,30 @@
 	}
 	
 	Color.prototype.step = function(){
-	  this.h >= 360 ? this.h = 0 : this.h += 2;
+	  this.h >= 360 ? this.h = 0 : this.h += .5;
 	
 	  if(this.lIncreasing){
-	    if(this.l >= 75){
+	    if(this.l >= 50){
 	      this.lIncreasing = false;
-	      this.l -= .2;
-	    } else this.l += .2;
+	      this.l -= .05;
+	    } else this.l += .05;
 	  } else {
-	    if(this.l <= 5){
+	    if(this.l <= 0){
 	      this.lIncreasing = true;
-	      this.l += .2;
-	    } else this.l -= .2;
+	      this.l += .05;
+	    } else this.l -= .05;
 	  }
 	
 	  // lightness could depend on how well we're doing
 	};
 	
 	Color.prototype.main = function () {
-	  const hsla = `hsla(${this.h}, ${this.s}%, ${this.l}%, 1)`;
+	  const hsla = `hsla(${this.h}, ${this.s}%, ${this.l + 50}%, 1)`;
 	  return hsla;
 	};
 	
 	Color.prototype.wave = function () {
-	  const hsla = `hsla(${this.h + 180}, ${this.s}%, ${this.l}%, 1)`;
+	  const hsla = `hsla(${this.h}, ${this.s}%, ${this.l}%, 1)`;
 	  return hsla;
 	};
 	
@@ -171,16 +270,16 @@
 
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const Point = __webpack_require__(5);
-	const Color = __webpack_require__(3);
-	const Player = __webpack_require__(6);
+	const Point = __webpack_require__(6);
+	const Color = __webpack_require__(4);
 	
 	function Wave(canvas) {
 	  this.canvas = canvas;
 	  this.spacing = 100;
+	  this.speed = 5;
 	  this.points = Point.generatePoints(
 	    canvas.width,
 	    canvas.height,
@@ -188,7 +287,14 @@
 	  );
 	}
 	
-	Wave.prototype.render = function(tide) {
+	Wave.prototype.move = function () {
+	  this.points.forEach( (point, i) => {
+	    point.move(this.speed);
+	  });
+	  this.keepPointsInBounds();
+	};
+	
+	Wave.prototype.render = function() {
 	  const ctx = this.canvas.ctx;
 	  const width = this.canvas.width;
 	  const height = this.canvas.height;
@@ -198,16 +304,12 @@
 	  ctx.beginPath();
 	  ctx.moveTo(this.points[0].x, this.points[0].y);
 	
-	  // split movement into separate function
 	  this.points.forEach( (point, i) => {
-	    point.move(tide);
 	    const nextPoint = this.points[i + 1];
 	    if (nextPoint) {
 	      ctx.lineTo(nextPoint.x, nextPoint.y);
 	    }
 	  });
-	
-	  this.keepPointsInBounds();
 	
 	  ctx.lineTo(width, height);
 	  ctx.lineTo(0, height);
@@ -220,16 +322,7 @@
 	  const points = this.points;
 	  const spacing = this.spacing;
 	
-	  if(points[points.length-1].x > this.canvas.width + spacing * 2){
-	    const newPoint = new Point(
-	      points[0].x - spacing,
-	      points[points.length-1].y,
-	      points[points.length-1].oldY
-	    );
-	    points.unshift(newPoint);
-	    points.pop();
-	
-	  } else if(points[0].x < (0 - spacing * 2)){
+	  if(points[0].x < (0 - spacing * 2)){
 	    const newPoint = new Point(
 	      points[points.length-1].x + spacing,
 	      points[0].y,
@@ -240,15 +333,11 @@
 	  }
 	};
 	
-	Wave.prototype.addPlayer = function () {
-	  return null;
-	};
-	
 	module.exports = Wave;
 
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports) {
 
 	function Point(x, y, oldY){
@@ -277,9 +366,9 @@
 	  return points;
 	};
 	
-	Point.prototype.move = function (tide) {
+	Point.prototype.move = function (speed) {
 	  this.y = this.oldY + Math.sin(this.angle) * this.amplitude;
-	  this.x -= 5;
+	  this.x -= speed;
 	  this.angle += this.speed;
 	};
 	
@@ -287,14 +376,39 @@
 
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports) {
 
-	function Player(){
-	  
+	const _viableKeys = [37, 38, 39, 40];
+	
+	function Listener(){
+	  this.keys = {};
+	
+	  document.addEventListener("keydown", e => this._keyDown(e));
+	  document.addEventListener("keyup", e => this._keyUp(e));
 	}
 	
-	module.exports = Player;
+	Listener.prototype._keyDown = function (e) {
+	  const code = e.keyCode;
+	  if(_viableKeys.includes(code)){
+	    e.preventDefault();
+	    this.keys[e.keyCode] = true;
+	  }
+	};
+	
+	Listener.prototype._keyUp = function (e) {
+	  const code = e.keyCode;
+	  if(_viableKeys.includes(code)){
+	    e.preventDefault();
+	    delete this.keys[code];
+	  }
+	};
+	
+	Listener.prototype.pressed = function (key) {
+	  return this.keys[key];
+	};
+	
+	module.exports = new Listener;
 
 
 /***/ }
